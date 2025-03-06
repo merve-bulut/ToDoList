6064776792bdb561e99ba0d03cc38d474c9cc5e7
Merve Bulut <47273479+merve-bulut@users.noreply.github.com>
2025-03-06 22:05:33 +0300

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Merve Bulut on 6.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
